30a98f0411da09fde923c72ab0b57e258a724134
Tomas Romero <canotto90@gmail.com>
2015-04-27 23:29:35 -0300

    Updated swal forms

--- package.js
+++ package.js
@@ -1,17 +1,14 @@
 Package.describe({
   name: 'canotto90:settings-conf-manager',
   summary: 'Lets you see and change app settings (both public and private)',
-  version: '0.1.1',
+  version: '0.1.2',
   git: 'https://github.com/taromero/settings-conf-manager',
   debugOnly: true
 })
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.1')
-  api.use([
-    'kevohagan:sweetalert@0.3.2',
-    'canotto90:swal-forms@0.0.5'
-  ], 'client')
+  api.use('canotto90:swal-forms@0.1.1', 'client')
   api.addFiles('client.js', 'client')
   api.addFiles('server.js', 'server')
 })
